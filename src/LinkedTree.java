1ef69293c71451cdb663f079fbe37210bb3a9425
Shaiful Islam <Shaiful Islam@DESKTOP-KS59TUP>
2018-12-16 14:12:18 +0100

    possible Solution

--- src/LinkedTree.java
+++ src/LinkedTree.java
@@ -1,53 +1,240 @@
 import java.util.LinkedList;
 
 public class LinkedTree<E> {
-    Node<E> root;
+	Node<E> root;
 
-    private class Node<E> {
-        private int key; //the key field
-        private LinkedList<E> data; //list of data associated with a key
-        private Node<E> left; //reference to the left child
-        private Node<E> right; //reference to the right child
-    
-        private Node(int key, E element, Node<E> leftChild, Node<E> rightChild) {
-            this.key = key;
-            this.data = new LinkedList<E>();
-            this.left = leftChild;
-            this.right = rightChild;
+	private class Node<E> {
+		private int key; // the key field
+		private LinkedList<E> data; // list of data associated with a key
+		private Node<E> left; // reference to the left child
+		private Node<E> right; // reference to the right child
 
-            this.data.add(element);
-        }
-    }
-    
-    public LinkedTree() {
-        //your implementation goes here
-    }
-    
-    public LinkedTree(int key, E element) {
-        //your implementation goes here
-    }
-    
-    public void insert(int key, E element) {
-        //your implementation goes here
-    }
-    
-    public LinkedList<E> search(int key) {
-        //your implementation goes here
-    }
-    
-    public void preorder(int ind) {
-        //your implementation goes here
-    }
+		private Node(int key, E element, Node<E> leftChild, Node<E> rightChild) {
+			this.key = key;
+			this.data = new LinkedList<E>();
+			this.left = leftChild;
+			this.right = rightChild;
 
-    public void postorder (int ind) {
-        //your implementation goes here
-    }
+			this.data.add(element);
+		}
 
-    public void inorder (int ind) {
-        //your implementation goes here
-    }
-    
-    public LinkedList<E> delete(int key) {
-        //your implementation goes here
-    }
+		private Node(int key, E element) {
+			this(key, element, null, null);
+		}
+	}
+
+	public LinkedTree() {
+		// your implementation goes here
+		System.out.println("init tree");
+		root = null;
+	}
+
+	public LinkedTree(int key, E element) {
+		// your implementation goes here
+		root = new Node<E>(key, element);
+	}
+
+	public void insert(int key, E element) {
+		// your implementation goes here
+		root = insert(root, key, element);
+	}
+
+	public LinkedList<E> search(int key) {
+		// your implementation goes here
+		Node<E> found = searchTree(root, key);
+
+		if (found != null) {
+			return found.data;
+		}
+
+		return null;
+	}
+
+	public void preorder(int ind) {
+		// your implementation goes here
+		traversePreorder(root, ind);
+		System.out.println();
+	}
+
+	public void postorder(int ind) {
+		// your implementation goes here
+		traversePostorder(root, ind);
+		System.out.println();
+	}
+
+	public void inorder(int ind) {
+		// your implementation goes here
+		traverseInorder(root, ind);
+		System.out.println();
+
+	}
+
+	public LinkedList<E> delete(int key) {
+		// your implementation goes here
+		LinkedList<E> data = null;
+
+		Node<E> node = searchTree(root, key);
+
+		// found
+		if (node != null) {
+
+			// extract data for returning
+			data = node.data;
+
+			// delete this node
+			root = remove(root, key);
+
+		}
+
+		return data;
+	}
+
+	// pvt. utility methods
+	private Node<E> searchTree(Node<E> node, int key) {
+
+		if (node == null) {
+			return null;
+		}
+
+		// search left
+		if (node.key > key) {
+			return searchTree(node.left, key);
+		}
+
+		// search right
+		if (node.key < key) {
+			return searchTree(node.right, key);
+		}
+
+		return node; // eql key
+	}
+
+	private void traverseInorder(Node<E> base, int idx) {
+
+		if (base == null) {
+			System.out.println("base node is null");
+			return;
+		}
+
+		// traverse left
+		if (base.left != null) {
+			traverseInorder(base.left, idx);
+		}
+
+		// traverse root
+		if (base.data != null && base.data.size() > idx) {
+
+			System.out.print(" " + base.data.get(idx) + " ");
+
+		}
+
+		// traverse right
+		if (base.right != null) {
+			traverseInorder(base.right, idx);
+		}
+	}
+
+	private void traversePreorder(Node<E> base, int idx) {
+
+		if (base == null) {
+			return;
+		}
+
+		// traverse root
+		if (base.data != null && base.data.size() > idx) {
+			System.out.print(" " + base.data.get(idx) + " ");
+		}
+
+		// traverse left
+		if (base.left != null) {
+			traversePreorder(base.left, idx);
+		}
+
+		// traverse right
+		if (base.right != null) {
+			traversePreorder(base.right, idx);
+		}
+	}
+
+	private void traversePostorder(Node<E> base, int idx) {
+
+		if (base == null) {
+			return;
+		}
+
+		// traverse left
+		if (base.left != null) {
+			traversePostorder(base.left, idx);
+		}
+
+		// traverse right
+		if (base.right != null) {
+			traversePostorder(base.right, idx);
+		}
+
+		// traverse root
+		if (base.data != null && base.data.size() > idx) {
+			System.out.print(" " + base.data.get(idx) + " ");
+		}
+	}
+
+	private Node<E> findLeftMostLeaf(Node<E> node) {
+		if (node == null) {
+			return null;
+		}
+
+		// go until leftmost node is found
+		if (node.left != null) {
+			return findLeftMostLeaf(node.left);
+		}
+
+		return node;
+	}
+
+	private Node<E> insert(Node<E> node, int key, E element) {
+
+		if (node == null) {
+			node = new Node<E>(key, element);
+		} else if (node.key > key) {
+			node.left = insert(node.left, key, element);
+		} else if (node.key < key) {
+			node.right = insert(node.right, key, element);
+		} else { // same node
+			node.data.add(element);
+		}
+
+		return node;
+	}
+
+	private Node<E> remove(Node<E> node, int key) {
+
+		if (node == null) {
+			return null;
+		}
+
+		if (node.key > key) {
+
+			node.left = remove(node.left, key);
+
+		} else if (node.key < key) {
+
+			node.right = remove(node.right, key);
+
+		} else if (node.left != null && node.right != null) {
+
+			Node<E> leftmost = findLeftMostLeaf(node.right);
+
+			// copy over
+			node.data = leftmost.data;
+			node.key = leftmost.key;
+
+			// delete
+			node.right = remove(node.right, node.key);
+
+		} else {
+			node = node.left == null ? node.right : node.left;
+		}
+
+		return node;
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,96 @@
+
 /**
  * Main class of the Java program. 
  * 
  */
+import java.util.LinkedList;
+import static org.junit.Assert.assertTrue;
 
 public class Main {
 
-    public static void main(String[] args) {
-        //test your implementation here
-    }
-}
+	public static void main(String[] args) {
+		// test your implementation here
+		LinkedTree<String> t = new LinkedTree();
 
+		t.insert(16, "R");
+		t.insert(16, "D");
+		t.insert(16, "T");
+
+		t.insert(8, "A");
+		t.insert(8, "C");
+		t.insert(8, "E");
+
+		t.insert(4, "D");
+		t.insert(4, "R");
+		t.insert(4, "M");
+
+		t.insert(15, "H");
+		t.insert(15, "O");
+		t.insert(15, "P");
+
+		t.insert(23, "M");
+		t.insert(23, "I");
+		t.insert(23, "L");
+
+		t.insert(42, "A");
+		t.insert(42, "H");
+		t.insert(42, "E");
+
+		int[] toDelete = new int[] { 4, 23, 16 };
+		LinkedList<String>[] data = new LinkedList[toDelete.length];
+
+		for (int i = 0; i < toDelete.length; ++i) {
+			data[i] = t.delete(toDelete[i]);
+			if (data[i] != null) {
+				System.out.println(data[i].toString() + " key: " + toDelete[i]);
+			}
+		}
+
+		// traverse
+		t.inorder(0);
+		t.postorder(1);
+		t.preorder(2);
+
+		// delete test
+		replicateDeleteTest();
+	}
+
+	public static void replicateDeleteTest() {
+
+		// inserts/ops from insertAndSearchTest()
+		LinkedTree<String> x = new LinkedTree();
+		x.insert(100, "Root");
+		x.insert(150, "Right");
+		x.insert(50, "Left");
+		x.insert(200, "RightRight");
+		x.insert(140, "RightLeft");
+		x.insert(20, "LeftLeft");
+		x.insert(70, "LeftRight");
+
+		// content of deleteTest()
+		LinkedList<String>[] elements = new LinkedList[4];
+
+		elements[0] = x.delete(100);
+		elements[1] = x.delete(20);
+		elements[2] = x.delete(150);
+		elements[3] = x.delete(200);
+
+		for (int i = 0; i < elements.length; i++) {
+			assertTrue(elements[i] != null && elements[i].size() == 1);
+		}
+
+		assertTrue(elements[0].get(0).equals("Root"));
+		assertTrue(elements[1].get(0).equals("LeftLeft"));
+		assertTrue(elements[2].get(0).equals("Right"));
+		assertTrue(elements[3].get(0).equals("RightRight"));
+
+		assertTrue(x.search(50) != null);
+		assertTrue(x.search(70) != null);
+		assertTrue(x.search(140) != null);
+
+		assertTrue(x.search(100) == null);
+		assertTrue(x.search(20) == null);
+		assertTrue(x.search(150) == null);
+		assertTrue(x.search(200) == null);
+	}
+}
